49cedcc12049555a7d804b4123d32fa566634eb2
Matthew Hamilton <matthamilton@live.com>
2018-01-15 21:17:48 -0600

    Initialize project.

---
+++ src/Simple.Migrations.DotNet.Tools/Program.cs
@@ -0,0 +1,26 @@
+﻿using SimpleMigrations;
+using SimpleMigrations.Console;
+using SimpleMigrations.DatabaseProvider;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Simple.Migrations.DotNet.Tools
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AssemblyMigrationProvider migrationAssembly = null;
+            DbConnection connection = null;
+            var provider = new MssqlDatabaseProvider(connection) { SchemaName = "dbo" };
+            var migrator = new SimpleMigrator(migrationAssembly, provider);
+            migrator.Load();
+
+            var runner = new ConsoleRunner(migrator);
+
+
+        }
+    }
+}
