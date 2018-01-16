166fe073aeb3bbc81e6785161685e5d37cfbf655
Matthew Hamilton <matthamilton@live.com>
2018-01-16 08:25:19 -0600

    More work.

--- src/Simple.Migrations.Tools.DotNet/Program.cs
+++ src/Simple.Migrations.Tools.DotNet/Program.cs
@@ -8,14 +8,14 @@ using System.Text;
 
 namespace Simple.Migrations.DotNet.Tools
 {
-    class Program
+    static class Program
     {
         public static void Main(string[] args)
         {
             AssemblyMigrationProvider migrationAssembly = null;
             DbConnection connection = null;
             var provider = new MssqlDatabaseProvider(connection) { SchemaName = "dbo" };
-            var migrator = new SimpleMigrator(migrationAssembly, provider);
+            var migrator = new SimpleMigrator(migrationAssembly, provider, new ConsoleLogger());
             migrator.Load();
 
             var runner = new ConsoleRunner(migrator);
